9a2c544a5da79bafd593b0c148444c510c403f7b
Mahnoor <you@example.com>
2024-08-18 12:45:17 +0000

    add

--- python.py
+++ python.py
@@ -24,3 +24,100 @@ print(fullName)
 
 
 
+
+a = b = c = 10 #single value assign with multiple variable
+
+# to find address variable with id 
+num12 = 300
+print(id(num12))
+# type casting
+print(type(num12))
+
+
+
+a,b,c = 10,20,30 #multiple value assign with multiple variable
+
+
+
+# reminder modulus
+r = 30%2
+print(r)
+# if divident value is smaller than divider
+f = 2%30
+print(f)
+# power square
+e = 2**4
+print(e)
+
+# floor division
+u = 10//3
+print(u)
+
+# add value to variable (add & assign)
+s = 50
+s += 50
+print(s)
+# floor division assign op 
+# multiply assign op
+# divion assign op
+# sub assign op
+
+
+# delete variable 
+num13 = 200
+del num13
+
+
+
+# modulus , floor division or exponention( who come first),
+# division multipliction , add or sum
+# num10 =3=3-10**4//2>4 (answer in true or false)
+
+# camparision operaters
+add = 4>=5
+print(add)
+
+# For AND operator 
+# Tand T = T
+# T and F = F
+#  F and T = F
+# F and T = F
+
+# For OR operator
+# F and T = T
+# F and F = F
+# T and T = T
+# T and F = T
+# For NOT operator
+# !T = F
+# !F = T
+
+
+user = int(input("hello amna , entre a number"))
+output = user + 3
+print(output)
+
+
+
+# integer to string
+num40 = 45
+num50 = str(num40)
+print(num50)
+
+
+
+user3:int = int(input("enter a number"))23
+output2 = int(user3) - 5
+print(output2)
+ 
+
+
+
+
+
+
+
+
+
+
+
